347915125378485dc5e74e094937cd6e7853543d
root <root@ip-172-31-18-5.ec2.internal>
2026-08-31 09:41:42 +0000

    java_project

---
+++ app.java
@@ -0,0 +1,10 @@
+public class app {
+	public static void main(string[]args){
+		int a=10;
+		int b=5;
+		system.out.println("addition is:"+(a+b));
+		system.out.println("difference is:"+(a-b));
+		system.out.println("product is:"+(a*b));
+		system.out.println("divsion is:"+(a/b));
+	}
+}
